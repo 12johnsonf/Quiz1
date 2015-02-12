bcdf9a006f77d77f24ab9c254cd1f3a0b70c9075
Felix Johnson <felixjohnson@hotmail.co.uk>
2015-02-12 16:46:31 +0000

    end.py
    dev stage 5

---
+++ end,py.py
@@ -0,0 +1,7 @@
+def scores():
+    for i in range(10):
+                j += 1
+            if questions [i][1] == questions[i][2]:
+                print(questions[i][1], questions[i][2] + "   RIGHT")
+            else:
+                print(questions[i][1], questions[i][2] + "   WRONG")
