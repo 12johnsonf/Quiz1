7c5d09e77f485df402543fd5cb961b6676023a64
12johnsonf <felixjohnson@hotmail.co.uk>
2015-02-12 16:48:49 +0000

    Update end.py

--- end.py
+++ end.py
@@ -2,6 +2,6 @@ def scores():
     for i in range(10):
                 j += 1
             if questions [i][1] == questions[i][2]:
-                print(questions[i][1], questions[i][2] + "   RIGHT")
+                print(questions[i][1], questions[i][2] + "   ✓")
             else:
-                print(questions[i][1], questions[i][2] + "   WRONG")
+                print(questions[i][1], questions[i][2] + "   ❌")
